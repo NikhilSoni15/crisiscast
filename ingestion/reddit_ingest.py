c8e7ed7c2d46a7fae7eef3b4afcc92532f29c9b5
Austin Huang <im@austinhuang.me>
2025-04-29 01:41:54 -0400

    chore: use read-only access to reddit without username & password

--- ingestion/reddit_ingest.py
+++ ingestion/reddit_ingest.py
@@ -15,9 +15,9 @@ reddit = praw.Reddit(
     client_id=os.getenv("REDDIT_CLIENT_ID"),
     client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
     user_agent=os.getenv("REDDIT_USER_AGENT"),
-    username=os.getenv("REDDIT_USERNAME"),
-    password=os.getenv("REDDIT_PASSWORD")
 )
+
+reddit.read_only = True
 
 try:
     print("✅ Logged in as:", reddit.user.me())
